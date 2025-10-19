de6486376deaa33fea51de65cffd566157fdf5ab
Vagner Montenegro de Melo <vagnermontenegro2@gmail.com>
2025-10-19 10:04:59 -0300

    consertei o ordem servico

--- src/br/edu/cs/poo/ac/ordem/entidades/OrdemServico.java
+++ src/br/edu/cs/poo/ac/ordem/entidades/OrdemServico.java
@@ -3,14 +3,16 @@ package br.edu.cs.poo.ac.ordem.entidades;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @Getter
 @Setter
+
 public class OrdemServico implements Serializable {
 
 	private Cliente cliente;
@@ -20,32 +22,33 @@ public class OrdemServico implements Serializable {
 	private LocalDateTime dataHoraAbertura;
 	private int prazoEmDias;
 	private double valor;
-	
+
 	public LocalDate getDataEstimadaEntrega() {
-		
-		LocalDate data = dataHoraAbertura.toLocalDate();
-		data = data.plusDays(prazoEmDias);
-		return data;
+
+		LocalDate dataAbertura = dataHoraAbertura.toLocalDate();
+
+		return dataAbertura.plusDays(prazoEmDias);
 	}
-	
+
 	public String getNumero() {
-		String Equipamento = "";
-		if(notebook.getIdTipo() != null) {
-			Equipamento = notebook.getIdTipo();
+
+		String concatenacao = "";
+		if (notebook != null) {
+			concatenacao += notebook.getIdTipo();
+		} else{
+			concatenacao += desktop.getIdTipo();
 		}
-		else if (desktop.getIdTipo() != null){
-			Equipamento = desktop.getIdTipo();
+
+	    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmm");
+	    concatenacao += dataHoraAbertura.format(formatter);
+
+		if (cliente.getCpfCnpj().length() > 11) {
+			concatenacao += cliente.getCpfCnpj();
+		} else {
+			concatenacao += "000" + cliente.getCpfCnpj();
 		}
-		
-		LocalDateTime data = dataHoraAbertura;
-		
-		if (cliente.getCpfCnpj() )
+
+		return concatenacao;
 	}
+
 }
-
-
-• Método público String getNumeroi(): retorna a concatenação do tipo de equipamento (retorno do
-método getIdTipo do notebook ou do desktop, quem não for nulo) ano, mês, dia, hora e minuto com o
-cpf ou cnpj do cliente, se o cpf ou cnpj do cliente for um cnpj; ou retorna a concatenação do mês,
-ano, dia, hora e minuto com “000” e com o cpf ou cnpj do cliente, se o cpf ou cnpj do cliente for um
-cnpj.
